6c3195b0c36fd8cf1537a323163c4af1eec0f185
AJ Ruf <z0isch@gmail.com>
2021-04-27 13:43:55 -0400

    Linter

--- src/helpers/graph/graph-quadratic-util.js
+++ src/helpers/graph/graph-quadratic-util.js
@@ -1,49 +1,84 @@
 /* @flow */
 
-import {getClosestStepPoint} from './../graph-util.js'
-import {getArrayOfNElements} from './../array-helper.js'
-import type {GraphSettingsT, PointT, GraphPropertiesT, QuadraticGraphPropertyT} from './../graph-util.js'
+import { getClosestStepPoint } from "./../graph-util.js";
+import { getArrayOfNElements } from "./../array-helper.js";
+import type {
+  GraphSettingsT,
+  PointT,
+  GraphPropertiesT,
+  QuadraticGraphPropertyT,
+} from "./../graph-util.js";
 
 const GraphQuadraticUtil = {
+  createGraph: function (
+    graph: any,
+    onPointChanged: (
+      movingPoint: ?PointT,
+      graphProperties: GraphPropertiesT
+    ) => void,
+    graphSettings: GraphSettingsT
+  ) {
+    const [vertexColor, pointColor] = getArrayOfNElements(
+      graphSettings.pointColors,
+      2
+    );
+    graph.createCircle(
+      "vertex",
+      graphSettings.startingPoints[0],
+      graphSettings.pointSize,
+      vertexColor
+    );
+    graph.createCircle(
+      "point",
+      graphSettings.startingPoints[1],
+      graphSettings.pointSize,
+      pointColor
+    );
 
-  createGraph: function(graph: any, onPointChanged: (movingPoint: ?PointT, graphProperties: GraphPropertiesT) => void, graphSettings: GraphSettingsT) {
+    graph.quadraticEquation.updateFunction();
 
-    const [vertexColor, pointColor] = getArrayOfNElements(graphSettings.pointColors, 2)
-    graph.createCircle('vertex', graphSettings.startingPoints[0], graphSettings.pointSize, vertexColor)
-    graph.createCircle('point', graphSettings.startingPoints[1], graphSettings.pointSize, pointColor)
-
-    graph.quadraticEquation.updateFunction()
-
-    const moveAndUpdate = function(movedPoint: PointT, {vertex, point}: QuadraticGraphPropertyT) {
-      const stepPoint = getClosestStepPoint(movedPoint, graphSettings)
-      const hasMoved = graph.quadraticEquation.moveDraggedItemAt(stepPoint)
+    const moveAndUpdate = function (
+      movedPoint: PointT,
+      { vertex, point }: QuadraticGraphPropertyT
+    ) {
+      const stepPoint = getClosestStepPoint(movedPoint, graphSettings);
+      const hasMoved = graph.quadraticEquation.moveDraggedItemAt(stepPoint);
       if (hasMoved) {
-        const graphProperties =
-          { graphType: 'quadratic'
-          , property:
-            { vertex: getClosestStepPoint(vertex, graphSettings)
-            , point: getClosestStepPoint(point, graphSettings)
-            }
-          }
-        onPointChanged(stepPoint, graphProperties)
-        graph.quadraticEquation.updateFunction()
+        const graphProperties = {
+          graphType: "quadratic",
+          property: {
+            vertex: getClosestStepPoint(vertex, graphSettings),
+            point: getClosestStepPoint(point, graphSettings),
+          },
+        };
+        onPointChanged(stepPoint, graphProperties);
+        graph.quadraticEquation.updateFunction();
       }
-    }
+    };
 
-    const onMouseDown = function(point: PointT, points: QuadraticGraphPropertyT) {
-      graph.quadraticEquation.startDraggingItemAt(point)
-      moveAndUpdate(point, points)
-    }
-    const onMouseMove = function(point: PointT, points: QuadraticGraphPropertyT) {
-      moveAndUpdate(point, points)
-    }
-    const onMouseUp = function(point: PointT, points: QuadraticGraphPropertyT) {
-      moveAndUpdate(point, points)
-      graph.quadraticEquation.stopDraggingItem()
-    }
+    const onMouseDown = function (
+      point: PointT,
+      points: QuadraticGraphPropertyT
+    ) {
+      graph.quadraticEquation.startDraggingItemAt(point);
+      moveAndUpdate(point, points);
+    };
+    const onMouseMove = function (
+      point: PointT,
+      points: QuadraticGraphPropertyT
+    ) {
+      moveAndUpdate(point, points);
+    };
+    const onMouseUp = function (
+      point: PointT,
+      points: QuadraticGraphPropertyT
+    ) {
+      moveAndUpdate(point, points);
+      graph.quadraticEquation.stopDraggingItem();
+    };
 
-    graph.quadraticEquation.setDraggable(onMouseDown, onMouseMove, onMouseUp)
-  }
-}
+    graph.quadraticEquation.setDraggable(onMouseDown, onMouseMove, onMouseUp);
+  },
+};
 
-export default GraphQuadraticUtil
+export default GraphQuadraticUtil;
