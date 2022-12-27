dbda6cc3a3b5bc4a6fb82663ac0e9a7e5703d7e3
Restyled.io <commits@restyled.io>
2022-12-27 14:44:10 +0000

    Restyled by prettier

--- src/helpers/graph-util.js
+++ src/helpers/graph-util.js
@@ -9,10 +9,15 @@ import GraphQuadraticUtil from "./graph/graph-quadratic-util.js";
 import GraphScatterPointsUtil from "./graph/graph-scatter-points-util.js";
 import PaperUtil from "./paper-util.js";
 
-export type GraphTypeT =|"linear"|"linear-inequality"|"quadratic"|"exponential"|
-    "scatter-points"|"empty";
+export type GraphTypeT =
+  | "linear"
+  | "linear-inequality"
+  | "quadratic"
+  | "exponential"
+  | "scatter-points"
+  | "empty";
 
-export type InequalityT = "lt"|"le"|"gt"|"ge";
+export type InequalityT = "lt" | "le" | "gt" | "ge";
 
 export type PointT = {
   x: number,
@@ -29,10 +34,11 @@ export type GraphSettingsT = {
   startingPoints: Array<PointT>,
   pointColors: Array<string>,
   pointSize: number,
-  inequality
-  ?: InequalityT, showBoundingLabels: ? boolean, canInteract : boolean,
-  ariaDescribedby?:
-      ? string};
+  inequality?: InequalityT,
+  showBoundingLabels: ?boolean,
+  canInteract: boolean,
+  ariaDescribedby?: ?string,
+};
 
 export type LinearGraphPropertyT = {
   points: Array<PointT>,
@@ -57,26 +63,31 @@ export type ScatterPointsGraphPropertyT = {
 };
 
 type GraphPropertyT =
-    |LinearGraphPropertyT|QuadraticGraphPropertyT|ExponentialGraphPropertyT|
-    LinearInequalityGraphPropertyT|ScatterPointsGraphPropertyT;
+  | LinearGraphPropertyT
+  | QuadraticGraphPropertyT
+  | ExponentialGraphPropertyT
+  | LinearInequalityGraphPropertyT
+  | ScatterPointsGraphPropertyT;
 
 export type GraphPropertiesT = {
   graphType: GraphTypeT,
   property: GraphPropertyT,
 };
 
-export function getClosestStepPoint(point: PointT,
-                                    graphSettings: GraphSettingsT): PointT {
-  const {stepX, stepY} = graphSettings;
+export function getClosestStepPoint(
+  point: PointT,
+  graphSettings: GraphSettingsT
+): PointT {
+  const { stepX, stepY } = graphSettings;
   return {
-    x : Math.round(point.x / stepX) * stepX,
-    y : Math.round(point.y / stepY) * stepY,
+    x: Math.round(point.x / stepX) * stepX,
+    y: Math.round(point.y / stepY) * stepY,
   };
 }
 
 const GraphUtil = {
   // This function traces the grid on the canvas based on the graph settings
-  createGrid : function(graph: any, graphSettings: GraphSettingsT) {
+  createGrid: function (graph: any, graphSettings: GraphSettingsT) {
     const {
       minGridX,
       maxGridX,
@@ -86,87 +97,111 @@ const GraphUtil = {
       stepY,
       showBoundingLabels,
     } = graphSettings;
-    const minXAxisGrid = {x : minGridX, y : 0};
-    const maxXAxisGrid = {x : maxGridX, y : 0};
-    const minYAxisGrid = {x : 0, y : minGridY};
-    const maxYAxisGrid = {x : 0, y : maxGridY};
+    const minXAxisGrid = { x: minGridX, y: 0 };
+    const maxXAxisGrid = { x: maxGridX, y: 0 };
+    const minYAxisGrid = { x: 0, y: minGridY };
+    const maxYAxisGrid = { x: 0, y: maxGridY };
     _.chain(_.range(minGridX, maxGridX, stepX))
-        .filter((xLineX) => xLineX !== 0)
-        .forEach((xLineX) =>
-                     graph.createLine("grid", {x : xLineX, y : minGridY},
-                                      {x : xLineX, y : maxGridY}, "#eeeeee"))
-        .value();
+      .filter((xLineX) => xLineX !== 0)
+      .forEach((xLineX) =>
+        graph.createLine(
+          "grid",
+          { x: xLineX, y: minGridY },
+          { x: xLineX, y: maxGridY },
+          "#eeeeee"
+        )
+      )
+      .value();
 
     _.chain(_.range(minGridY, maxGridY, stepY))
-        .filter((yLineY) => yLineY !== 0)
-        .forEach((yLineY) =>
-                     graph.createLine("grid", {x : minGridX, y : yLineY},
-                                      {x : maxGridX, y : yLineY}, "#eeeeee"))
-        .value();
+      .filter((yLineY) => yLineY !== 0)
+      .forEach((yLineY) =>
+        graph.createLine(
+          "grid",
+          { x: minGridX, y: yLineY },
+          { x: maxGridX, y: yLineY },
+          "#eeeeee"
+        )
+      )
+      .value();
 
     graph.createLine("grid", minXAxisGrid, maxXAxisGrid, "black");
     graph.createLine("grid", minYAxisGrid, maxYAxisGrid, "black");
 
     if (showBoundingLabels) {
-
       // arrived at -4 through trial and error, observing the point where the
       // cutoff from the bottom started
       if (minGridY > -4) {
-        graph.createLabel('label', minXAxisGrid, minGridX.toString(),
-                          [ 'bottom', 'left' ]);
-        graph.createLabel('label', maxXAxisGrid, maxGridX.toString(),
-                          [ 'bottom', 'right' ]);
+        graph.createLabel("label", minXAxisGrid, minGridX.toString(), [
+          "bottom",
+          "left",
+        ]);
+        graph.createLabel("label", maxXAxisGrid, maxGridX.toString(), [
+          "bottom",
+          "right",
+        ]);
       } else {
-        graph.createLabel('label', minXAxisGrid, minGridX.toString(),
-                          [ 'left' ]);
-        graph.createLabel('label', maxXAxisGrid, maxGridX.toString(),
-                          [ 'right' ]);
+        graph.createLabel("label", minXAxisGrid, minGridX.toString(), ["left"]);
+        graph.createLabel("label", maxXAxisGrid, maxGridX.toString(), [
+          "right",
+        ]);
       }
-      graph.createTick('tick', maxXAxisGrid, 'x');
-      graph.createTick('tick', minXAxisGrid, 'x');
+      graph.createTick("tick", maxXAxisGrid, "x");
+      graph.createTick("tick", minXAxisGrid, "x");
 
-      graph.createLabel("label", minYAxisGrid, minGridY.toString(),
-                        [ "bottom" ]);
+      graph.createLabel("label", minYAxisGrid, minGridY.toString(), ["bottom"]);
       graph.createTick("tick", minYAxisGrid, "y");
 
-      graph.createLabel("label", maxYAxisGrid, maxGridY.toString(), [ "left" ]);
+      graph.createLabel("label", maxYAxisGrid, maxGridY.toString(), ["left"]);
       graph.createTick("tick", maxYAxisGrid, "y");
     }
   },
 
   // This function initialize the canvas with the drawing library,
   // set the grid on the canvas and set the default points for the graph
-  setupGraph : function(graphType: GraphTypeT, canvas: any,
-                        onPointChanged: (movingPoint:    ? PointT,
-                                         graphProperties : GraphPropertiesT) =>
-                            void,
-                        graphSettings: GraphSettingsT) : any {
+  setupGraph: function (
+    graphType: GraphTypeT,
+    canvas: any,
+    onPointChanged: (
+      movingPoint: ?PointT,
+      graphProperties: GraphPropertiesT
+    ) => void,
+    graphSettings: GraphSettingsT
+  ): any {
     const graph = PaperUtil.setupGraph(canvas, graphSettings);
 
     GraphUtil.createGrid(graph, graphSettings);
 
     switch (graphType) {
-    case "linear":
-      GraphLinearUtil.createGraph(graph, onPointChanged, graphSettings);
-      break;
-    case "linear-inequality":
-      GraphLinearInequalityUtil.createGraph(graph, onPointChanged,
-                                            graphSettings);
-      break;
-    case "quadratic":
-      GraphQuadraticUtil.createGraph(graph, onPointChanged, graphSettings);
-      break;
-    case "exponential":
-      GraphExponentialUtil.createGraph(graph, onPointChanged, graphSettings);
-      break;
-    case "scatter-points":
-      GraphScatterPointsUtil.createGraph(graph, onPointChanged, graphSettings);
-      break;
-    case "empty":
-      break;
-    default:
-      throw new Error(
-          `Could not recognize graph type: ${this.props.graphType}`);
+      case "linear":
+        GraphLinearUtil.createGraph(graph, onPointChanged, graphSettings);
+        break;
+      case "linear-inequality":
+        GraphLinearInequalityUtil.createGraph(
+          graph,
+          onPointChanged,
+          graphSettings
+        );
+        break;
+      case "quadratic":
+        GraphQuadraticUtil.createGraph(graph, onPointChanged, graphSettings);
+        break;
+      case "exponential":
+        GraphExponentialUtil.createGraph(graph, onPointChanged, graphSettings);
+        break;
+      case "scatter-points":
+        GraphScatterPointsUtil.createGraph(
+          graph,
+          onPointChanged,
+          graphSettings
+        );
+        break;
+      case "empty":
+        break;
+      default:
+        throw new Error(
+          `Could not recognize graph type: ${this.props.graphType}`
+        );
     }
     return graph;
   },
